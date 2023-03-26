d3c1b3400f5858562b2f8bc383858f53273120b1
samuk <sam@bristolwireless.net>
2023-03-26 23:22:32 +0100

    Update Smartcar_IMU.h

--- src/sensors/heading/Smartcar_IMU/Smartcar_IMU.h
+++ src/sensors/heading/Smartcar_IMU/Smartcar_IMU.h
@@ -1,22 +1,24 @@
 #ifndef SMARTCAR_IMU_H
 #define SMARTCAR_IMU_H
 
-#include <Adafruit_BNO08x.h>
+#if defined(USE_ADAFRUIT_BNO08x)
+  #include <Adafruit_BNO08x.h>
+  #include <Adafruit_Sensor.h>
+#elif defined(USE_MPU9250_WE)
+  #include <MPU9250_WE.h>
+#endif
 
-class Smartcar_IMU : public Adafruit_BNO08x {
-public:
-  Smartcar_IMU(TwoWire *wire = &Wire, int32_t sensorID = -1)
-      : Adafruit_BNO08x(sensorID, wire) {}
-  void begin() { Adafruit_BNO08x::begin(); }
-  void setRotationVector(uint16_t interval_ms) {
-    Adafruit_BNO08x::enableRotationVector(interval_ms);
-  }
-  float getHeading() {
-    imu::Quaternion quat = getQuat();
-    imu::Vector<3> euler = quat.toEuler();
-    float heading = euler.y() * 180.0 / M_PI;
-    return heading;
-  }
+class Smartcar_IMU {
+  public:
+    Smartcar_IMU();
+    void begin();
+    float getHeading();
+  private:
+    #if defined(USE_ADAFRUIT_BNO08x)
+      Adafruit_BNO08x _imu;
+    #elif defined(USE_MPU9250_WE)
+      MPU9250_WE _imu;
+    #endif
 };
 
-#endif // SMARTCAR_IMU_H
+#endif
